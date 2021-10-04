419ae6a96acf66b127b4c64788313d371d79074e
Sergey-10dev <Sergey-10dev@gmail.com>
2021-10-04 16:32:25 +0300

    feat: implement logic of adding and filtering tasks

--- src/components/TasksLists/TasksLists.js
+++ src/components/TasksLists/TasksLists.js
@@ -4,13 +4,32 @@ import { TaskListWrapper } from './Styled/TaskListWrapper';
 import { Title } from '../Title/Title';
 import { TaskAddition } from '../TaskAddition/TaskAddition';
 import { TaskSearch } from '../TaskSearch/TaskSearch';
+import { Task } from '../Task/Task';
 
 export const TasksLists = ({
-  tasks, filterText, onHandleAddTask, onHandleSearch,
-}) => (
-  <TaskListWrapper>
-    <Title />
-    <TaskAddition handleAddTask={onHandleAddTask} />
-    <TaskSearch handleSearch={onHandleSearch} />
-  </TaskListWrapper>
-);
+  tasks, onHandleAddTask, onHandleRemoveTask, onHandleComplition, onHandleSearch, filterText,
+}) => {
+  const handledTasks = tasks.filter((task) => task.text.indexOf(filterText, 0) !== -1);
+  return (
+    <TaskListWrapper>
+      <Title />
+      <TaskAddition handleAddTask={onHandleAddTask} />
+      {
+              handledTasks.map((task) => {
+                const Id = nanoid();
+                return (
+                  <Task
+                    handleRemoveTask={onHandleRemoveTask}
+                    handleComplition={onHandleComplition}
+                    key={Id}
+                    text={task.text}
+                    complited={task.complited}
+                    id={task.id}
+                  />
+                );
+              })
+          }
+      <TaskSearch handleSearch={onHandleSearch} />
+    </TaskListWrapper>
+  );
+};
